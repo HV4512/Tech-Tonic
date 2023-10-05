8937144e4982199dcf73b44fa0580bbc66e32176
Harsh Vardhan <jobbykarn@gmail.com>
2023-10-05 18:35:05 +0530

    Tonic Card Addition
    Tonic Card Structure Created
    
    ThreadCard.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,42 @@
 //"use client"
 
+import TonicCard from "@/components/cards/TonicCard";
 import { fetchTonics } from "@/lib/actions/tonic.actions";
+import Tonic from "@/lib/models/tonic.model";
+import { currentUser } from "@clerk/nextjs";
 
 const Home = async() => {
 
   const result = await fetchTonics(1,30);
+  const user = await currentUser();
   console.log(result);
   return (
     <>
-       <h1 className="head-text text-left text-green-500">
-         Home
-       </h1>
        <section className="mt-9 flex flex-col gap-10">
         {result.posts.length===0?(
           <p className="no-result">No Tonics Found </p>
         ):(
           <>
-          <p className="font-bold text-blue">Tonics Found</p>
+          {result.posts.map((tonic)=>(
+            <TonicCard
+            key={tonic._id}
+            id={tonic._id}
+            currentUserID={user?.id || ""}
+            parentId={tonic.parentId}
+            content={tonic.text}
+            author={tonic.author}
+            community={tonic.community}
+            createdAt={tonic.createdAt}
+            comments={tonic.children}
+            />
+
+          ))}
           </>
         )
       }
 
        </section>
     </>
-    // <>
-    // <h1 className="text-green-500">Home</h1>
-    // </>
     
   )
 }
---
+++ components/cards/TonicCard.tsx
@@ -0,0 +1,116 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+    id: string;
+    currentUserID: string;
+    parentId: string | null;
+    content: string;
+    author: {
+        name: string;
+        image: string;
+        id: string;
+    };
+    community: {
+        name: string;
+        image: string;
+        id: string;
+    } | null;
+    createdAt: string;
+    comments: {
+        author: {
+            image: string;
+        }
+    }[]
+    isComment?: boolean;
+}
+
+const TonicCard = ({
+    id,
+    currentUserID,
+    parentId,
+    content,
+    author,
+    community,
+    createdAt,
+    comments,
+    isComment,
+}: Props) => {
+    return (
+        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    {/* Profile Picture Segment */}
+                    <div className="flex flex-col items-center">
+                        <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                            <Image
+                                src={author.image}
+                                alt="Profile Picture"
+                                fill
+                                className="cursor-pointer rounded-full"
+                            />
+                        </Link>
+                        <div className="thread-card_bar" />
+                    </div>
+                    {/* Author's Name */}
+                    <div className="flex w-full flex-col">
+                        <Link href={`/profile/${author.id}`} className="w-fit ">
+                            <h4 className="cursor-pointer text-base-semibold text-blue hover:text-purple-500">{author.name}</h4>
+                            {/* <h4 className="cursor-pointer text-base-semibold text-light-1 mt-[8px]">{author.name}</h4> */}
+                        </Link>
+
+                        {/* Content of the Tonic */}
+                        <p className="mt-2 text-small-regular text-light-2">{content}</p>
+
+                        {/* Social Media Functions */}
+                        <div className="mt-5 flex flex-col gap-3">
+                            <div className="flex gap-3.5">
+                            <Image
+                  src='/assets/heart-gray.svg'
+                  alt='heart'
+                  width={24}
+                  height={24}
+                  className='cursor-pointer object-contain'
+                />
+
+                {/* Only reply works */}
+                <Link href={`/tonic/${id}`}>
+                  <Image
+                    src='/assets/reply.svg'
+                    alt='reply'
+                    width={24}
+                    height={24}
+                    className='cursor-pointer object-contain'
+                  />
+                </Link>
+
+                <Image
+                  src='/assets/repost.svg'
+                  alt='repost'
+                  width={24}
+                  height={24}
+                  className='cursor-pointer object-contain'
+                />
+                <Image
+                  src='/assets/share.svg'
+                  alt='share'
+                  width={24}
+                  height={24}
+                  className='cursor-pointer object-contain'
+                />
+
+                            </div>
+                            {isComment && comments.length>0 && (
+                                <Link href={`/tonic/${id}`}>
+                                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default TonicCard
